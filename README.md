188cb027490d49163c362448a79dfae73a630df7
L.Walker <logantwalker95@gmail.com>
2020-10-22 20:24:51 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# HW-19-EmployeeDirectory
-React Directory used for filtering and sorting a list of employees' information
---
+++ precache-manifest.7e71b475c8d85408c6a8d1e43acd9d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db8eaef8904c097d242ebcf8331c8f8",
+    "url": "/HW-19-EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "31c4c5b329cb977c1916",
+    "url": "/HW-19-EmployeeDirectory/static/css/main.8bc4f57d.chunk.css"
+  },
+  {
+    "revision": "4076f0b3b02145810912",
+    "url": "/HW-19-EmployeeDirectory/static/js/2.264ae9f3.chunk.js"
+  },
+  {
+    "revision": "39af20fba4c11006a97657c427f1b256",
+    "url": "/HW-19-EmployeeDirectory/static/js/2.264ae9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31c4c5b329cb977c1916",
+    "url": "/HW-19-EmployeeDirectory/static/js/main.2ba50c54.chunk.js"
+  },
+  {
+    "revision": "cbce0a01caad4a18bc92",
+    "url": "/HW-19-EmployeeDirectory/static/js/runtime-main.696c1671.js"
+  }
+]);
